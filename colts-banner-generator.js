aad833c167f7efa010ac909d52c7e3fb7ff53dae
Andrew Brown <arbrown83@gmail.com>
2022-11-14 15:24:39 -0500

    Colts Banner - multiple lines.

--- colts-banner-generator.js
+++ colts-banner-generator.js
@@ -11,12 +11,13 @@ function drawImage(banner_text) {
 
         // Add banner text.
         let bottom = getLines(ctx, banner_text, 250);
+
         ctx.fillStyle = 'rgb(255, 255, 255)';
         ctx.font = '105px Arial';
         ctx.textAlign = 'left';
         ctx.setTransform (1, 0.2, 0, 1, 0, 0);
         bottom.forEach(function(line, i) {
-            ctx.fillText(line.toUpperCase(), 500, bottom_start+(i*60),);
+            ctx.fillText(line.toUpperCase(), 500, 65+(i*60),);
         });
         ctx.setTransform (1, 0, 0, 1, 0, 0);
 
